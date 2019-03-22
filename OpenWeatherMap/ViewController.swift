78075d27cfe07c84ace5623cd0ba2c8dd1f1f2ed
John Goh <john.goh.ce@gmail.com>
2019-03-22 18:26:57 +0800

    Initial Commit

---
+++ OpenWeatherMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenWeatherMap
+//
+//  Created by John Goh on 22/3/19.
+//  Copyright © 2019 John Goh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
